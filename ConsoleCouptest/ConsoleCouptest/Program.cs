63d2ce1a7780b8140f6368f62dba84a8f840b2f9
eak0546 <57831268+eak0546@users.noreply.github.com>
2020-10-29 17:05:06 +0700

    Create Program.cs

---
+++ ConsoleCouptest/ConsoleCouptest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleCouptest;
+using CoupCardDeck;
+
+namespace ConsoleCoup
+{
+    class Program
+    {
+        
+
+        static void Main()
+        {
+            Player p = new Player();
+            Deck deck = new Deck();
+            deck.Shuffle();
+            p.player();
+
+        }
+    }
+}
