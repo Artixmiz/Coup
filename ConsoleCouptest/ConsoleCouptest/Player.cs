594abfea333b1f35f2d78323ea14049922c5aecb
eak0546 <57831268+eak0546@users.noreply.github.com>
2020-10-29 17:05:11 +0700

    Create Player.cs

---
+++ ConsoleCouptest/ConsoleCouptest/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoupCardDeck;
+
+namespace ConsoleCouptest
+{
+    public class Player
+
+    {
+        public readonly List<Card> card1;
+        public readonly List<Card> card2;
+        public void player()
+        {
+            foreach (var value in Enum.GetValues(typeof(CoupCardDeck.Action)))
+            {
+                Console.WriteLine((CoupCardDeck.Action)value);
+            }
+        }
+
+
+    }
+        
+}
