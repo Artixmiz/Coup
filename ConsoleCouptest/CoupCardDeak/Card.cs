7759108f6068ed88194d0083e1cef76569bf5a66
eak0546 <57831268+eak0546@users.noreply.github.com>
2020-10-29 17:04:58 +0700

    Create Card.cs

---
+++ ConsoleCouptest/CoupCardDeak/Card.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoupCardDeck
+{
+
+    public enum Action
+    {
+        Income,
+        Foreignaid,
+        Coup,
+        CollectTaxesd,
+        Exchange,
+        Assassinate,
+        Investigate,
+        Steal,
+        Extortion,
+        TargettedAction
+    }
+    public class Card
+    {
+
+        public string name;
+        public Action action;
+
+    }
+
+}
+
+
+
+
+
